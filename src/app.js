1f1888f54225271e565969cb2aad1f494bf1868c
Hippo-Cheese <jlogan466@@gmail.com>
2024-06-18 15:39:17 +0100

    added the current time

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ let currentDate = new Date();
 
 function displayDate() {
   const dateDisplay = document.getElementById('time-display');
-  dateDisplay.innerText = currentDate.toLocaleDateString(); 
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  dateDisplay.innerText = currentDate.toLocaleString('en-US', options);
 }
 
 function moveForward() {
